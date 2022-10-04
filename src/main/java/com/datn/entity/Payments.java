dd4a5f3ae55add7936f46494aceacac588a07eab
nguyenvantuanDEV <110893485+tuan2910@users.noreply.github.com>
2022-10-04 23:45:13 +0700

    update base

---
+++ src/main/java/com/datn/entity/Payments.java
@@ -0,0 +1,52 @@
+package com.datn.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "payments")
+public class Payments {
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "active")
+    private Boolean active;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return this.code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Boolean getActive() {
+        return this.active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
