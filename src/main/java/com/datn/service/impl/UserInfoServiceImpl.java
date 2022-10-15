4dbbaf68ab47592a3100018a5038fe39b60f1250
DBHieu <hieudbph14097@fpt.edu.vn>
2022-10-16 01:07:30 +0700

    check login updateProfile

--- src/main/java/com/datn/service/impl/UserInfoServiceImpl.java
+++ src/main/java/com/datn/service/impl/UserInfoServiceImpl.java
@@ -1,16 +1,19 @@
 package com.datn.service.impl;
 
 import com.datn.dto.admin.user.personal.PersonalDto;
+import com.datn.dto.customer.user.ProfileDto;
 import com.datn.service.UserInfoService;
 import com.datn.utils.base.PuddyException;
 import com.datn.utils.base.PuddyRepository;
 import com.datn.utils.base.rest.CurrentUser;
 import com.datn.utils.base.rest.ResData;
+import com.datn.utils.common.JsonUtils;
 import com.datn.utils.constants.PuddyCode;
 import com.datn.utils.validator.auth.AuthValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lombok.var;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -37,4 +40,21 @@ public class UserInfoServiceImpl implements UserInfoService {
 				.phone(user.getPhone())
 				.build());
 	}
+
+	@Override
+	public Object updateProfile(CurrentUser currentUser, ProfileDto dto) {
+		log.info("start updateProfile with payload: {}", JsonUtils.toJson(dto));
+
+		AuthValidator.checkLogin(currentUser);
+		var user = repository.userRepository.findByIdAndActive(dto.getId(), true);
+		if (null == user) {
+			throw new PuddyException(PuddyCode.INTERNAL_SERVER);
+		}
+		if (!currentUser.getId().equals(dto.getId())) {
+			throw new PuddyException(PuddyCode.FORBIDDEN);
+		}
+
+		return ResData.ok(user.getId());
+	}
+
 }
