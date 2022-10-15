aae07af3b69fe736bf81c2fbb9026736b644d743
DBHieu <hieudbph14097@fpt.edu.vn>
2022-10-16 01:02:57 +0700

    updateProfile

--- src/main/java/com/datn/service/UserInfoService.java
+++ src/main/java/com/datn/service/UserInfoService.java
@@ -1,8 +1,10 @@
 package com.datn.service;
 
+import com.datn.dto.customer.user.ProfileDto;
 import com.datn.utils.base.rest.CurrentUser;
 
 public interface UserInfoService {
 
+	Object updateProfile(CurrentUser currentUser, ProfileDto dto);
 	Object personal(CurrentUser currentUser);
 }
