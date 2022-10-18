d8982ff18379988d8c1b3c6b7216695f58327d0b
maitung82 <111722642+maitung268@users.noreply.github.com>
2022-10-18 21:09:44 +0700

    create color service

--- src/main/java/com/datn/service/ColorService.java
+++ src/main/java/com/datn/service/ColorService.java
@@ -2,11 +2,18 @@ package com.datn.service;
 
 
 import com.datn.dto.customer.product.ColorResponse;
+import com.datn.utils.base.enum_dto.ColorDto;
+import com.datn.utils.base.rest.CurrentUser;
 import com.datn.utils.base.rest.ResData;
 
 import java.util.List;
 
 public interface ColorService {
     ResData<List<ColorResponse>> getListColor();
+    ResData<String> create(CurrentUser currentUser, ColorDto dto);
+
+    ResData<String> delete(CurrentUser currentUser, ColorDto dto);
+
+    ResData<String> update(CurrentUser currentUser, ColorDto dto);
 
 }
