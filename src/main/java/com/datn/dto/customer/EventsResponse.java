d8cdb640b557e96d5487482727bc7f9b9a71e8b4
DBHieu <hieudbph14097@fpt.edu.vn>
2022-11-04 19:11:38 +0700

    feat/eventsReponse

---
+++ src/main/java/com/datn/dto/customer/EventsResponse.java
@@ -0,0 +1,20 @@
+package com.datn.dto.customer;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EventsResponse {
+	private Long eventId;
+	private String eventName;
+	private String status;
+	private Date startDate;
+	private String enDate;
+}
