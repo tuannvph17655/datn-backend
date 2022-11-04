d85b200333db7e9e937f207bf67ab9f027fc9e3a
DBHieu <hieudbph14097@fpt.edu.vn>
2022-11-04 20:15:21 +0700

    feat/getAllEvent

--- src/main/java/com/datn/utils/base/PuddyRepository.java
+++ src/main/java/com/datn/utils/base/PuddyRepository.java
@@ -25,4 +25,6 @@ public class PuddyRepository {
     public final OrderDetailRepository orderDetailRepository;
     public final OrderStatusRepository orderStatusRepository;
 
+    public final EventsRepository eventsRepository;
+
 }
