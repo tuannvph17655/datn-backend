3882c846e32dc48e3ef6d2c4c75029a2d1156a32
Nguyễn Đức Huy <huyndph17533@fpt.edu.vn>
2022-11-06 22:13:49 +0700

    suggest validate

---
+++ src/main/java/com/datn/utils/validator/suggest/SuggestValidator.java
@@ -0,0 +1,39 @@
+package com.datn.utils.validator.suggest;
+
+import com.datn.dto.customer.suggest.SuggestDto;
+import com.datn.utils.common.StringUtils;
+import com.datn.utils.constants.PuddyCode;
+import com.datn.utils.constants.PuddyException;
+
+/**
+ * @author myname
+ */
+public class SuggestValidator {
+    public static void validGetSizeAvailable(SuggestDto dto) {
+        if (StringUtils.isNullOrEmpty(dto.getCategoryId())) {
+            throw new PuddyException(PuddyCode.BAD_REQUEST, "Không được để trống loại sản phẩm");
+        }
+        if (StringUtils.isNullOrEmpty(dto.getWeight())) {
+            throw new PuddyException(PuddyCode.BAD_REQUEST, "Không được để trống cân nặng");
+        }
+        if (StringUtils.isNullOrEmpty(dto.getHeight())) {
+            throw new PuddyException(PuddyCode.BAD_REQUEST, "Không được để trống chiều cao");
+        }
+        try {
+            var weight = Long.valueOf(dto.getWeight());
+            if (weight < 41 || weight > 105) {
+                throw new PuddyException(PuddyCode.BAD_REQUEST, "Cân nặng không hợp lệ");
+            }
+        } catch (Exception e) {
+            throw new PuddyException(PuddyCode.BAD_REQUEST, "Cân nặng không hợp lệ");
+        }
+        try {
+            var height = Long.valueOf(dto.getHeight());
+            if (height < 149 || height > 195) {
+                throw new PuddyException(PuddyCode.BAD_REQUEST, "Chiều cao không hợp lệ");
+            }
+        } catch (Exception e) {
+            throw new PuddyException(PuddyCode.BAD_REQUEST, "Chiều cao không hợp lệ");
+        }
+    }
+}
